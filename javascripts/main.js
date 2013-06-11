39198b4694b4e7bc64a05e2c4af25d9e80efff94
Adam Kaplan <akaplan@gilt.com>
2013-06-11 15:01:46 -0400

    Redesigned page to comply with Gilt Tech common theme, and provide repo/org details.

---
+++ javascripts/main.js
@@ -0,0 +1,109 @@
+var GH_API_URI = "https://api.github.com"
+
+var REPOS_PER_ROW = 3;
+
+var repos = [ ];
+
+$(document).ready(function() {
+  loadRepos(1);
+  updateMembers();
+});
+
+function loadRepos(page) {
+  $.getJSON(GH_API_URI + "/orgs/gilt/repos?per_page=100&page=" + page,
+    function(result) {
+      if (result && result.length > 0) {
+        repos = repos.concat(result);
+        loadRepos(page + 1);
+      }
+      else {
+        addRepos(repos);
+      }
+    });
+}
+
+function addRepos(repos) {
+  var watchWeight = 6; // repo watchers
+  var forkWeight = 3; // forks of the repo
+  var giltWeight = 1000000;  // if the gilt repo is actually a fork
+  //var activityWeight = 0;
+
+  // Sort weight priority: gilt repo, starred, watched, activity
+  $.each(repos, function(i,repo) { // assign weights
+    var weight =
+      (repo.watchers_count * watchWeight) +
+      (repo.forks_count * forkWeight) +
+      (!repo.fork * giltWeight);
+    repo["gilt_weight"] = weight;
+  });
+  console.log(repos);
+
+  repos = repos.sort(function(a,b) {
+    var aw = a["gilt_weight"];
+    var bw = b["gilt_weight"];
+    if (aw == bw) {
+      return 0;
+    } else if (aw < bw) {
+      return 1;
+    }
+    else {
+      return -1;
+    }
+  });
+
+  $("#loading").addClass("hidden");
+
+  $.each(repos, function(i,repo) {
+    addRepo(i, repo);
+  });
+
+  // show repo stats
+  var stats = $("#repo-stats").text("Providing ");
+  $("<a>").attr("href", "https://github.com/gilt").text(repos.length + " public repositories").appendTo(stats);
+  stats.removeClass("hidden");
+}
+
+function addRepo(i, repo) {
+  var row = $("#all-repos").children().last();
+  if (! row || i % REPOS_PER_ROW == 0) {
+    row = $("<div>").addClass("repo-row row-fluid");
+    row.appendTo("#all-repos");
+  }
+
+  var r = $("<div>").addClass("repo span4");
+  var a = $("<a>").attr("href", repo.html_url).appendTo(r);
+
+  $("<i>").addClass("icon-star repo-icon").appendTo(a);
+  $("<span>").addClass("count").text(repo.watchers_count).appendTo(a);
+
+  $("<i>").addClass("icon-code-fork repo-icon").appendTo(a);
+  $("<span>").addClass("count").text(repo.forks_count).appendTo(a);
+
+  if (repo.private) {
+    $("<i>").addClass("icon-lock").appendTo(a);
+  }
+
+  var lang = repo.language;
+  if (lang) {
+    $("<span>").addClass("lang " + lang.toLowerCase()).text(lang).appendTo(a);
+  }
+
+  $("<h4>").addClass("name").text(repo.name).appendTo(a);
+  $("<p>").addClass("description").text(repo.description).appendTo(a);
+
+  r.appendTo(row);
+}
+
+function updateMembers() {
+  $.getJSON(GH_API_URI + "/orgs/gilt/members", function(result) {
+    if (result && result.length > 0) {
+      var stats = $("#member-stats").text("We are ");
+      $("<a>").attr("href", "https://github.com/gilt?tab=members").text(result.length + " members").appendTo(stats);
+      stats.removeClass("hidden");
+
+      // The "Providing N repos" message is designed to be displayed standalone
+      // add style to match up the letter case
+      $("#repo-stats").addClass("repo-stats-inline");
+    }
+  });
+}
